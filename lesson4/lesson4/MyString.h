bcbb3e1e2463ebd7bc05cfe000765961e8b19a85
בן סימון הילה גאולה <   @mbjcomp.local>
2025-11-23 12:46:40 +0200

    lesson4

---
+++ lesson4/lesson4/MyString.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+
+class MyString {
+private:
+    char* data;
+    int size;
+    static const int maxLen = 100;
+    static int instanceCount;
+public:
+    // 1
+    MyString();
+
+    // 2
+    MyString(const char* s, int size);
+
+    // 3
+    MyString(const MyString& other);
+
+    // 4
+    MyString& operator=(const MyString& other);
+
+    // 5
+    MyString(MyString&& obj) noexcept;
+
+    // 6
+    MyString& operator=(MyString&& other) noexcept;
+
+    // 7
+    ~MyString();
+
+    // 8
+    const char* getData() const;
+
+    // 9
+    void setData(const char* new_data, int size);
+
+    // 10
+    static int getInstanceCount();
+};
+
---
+++ lesson4/lesson4/lesson4.cpp
@@ -0,0 +1,39 @@
+﻿// lesson4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "MyString.h"
+#include <iostream>
+
+int main()
+{
+    try {
+        MyString s1("Jerusalem", 10);
+        MyString s2("Is", 3);
+        MyString s3("My Home", 8);
+
+        std::cout << "1: " << s1.getData() << std::endl;
+        std::cout << "2: " << s2.getData() << std::endl;
+        std::cout << "3: " << s3.getData() << std::endl;
+
+        s1 = s2;
+        std::cout << "After assignment, 1: " << s1.getData() << std::endl;
+
+        MyString s4 = s3;
+        std::cout << "4 (copy of 3): " << s4.getData() << std::endl;
+
+        MyString s5 = std::move(s4);
+        std::cout << "5 (moved from 4): " << s5.getData() << std::endl;
+
+        std::cout << "Current instance count: " << MyString::getInstanceCount() << std::endl;
+
+        MyString s6("This string is too long This string is too long This string is too long This string is too long This string is too long", 120);
+    }
+    catch (const std::length_error& error) {
+        std::cerr << "Exception: " << error.what() << std::endl;
+    }
+
+    std::cout << "Finally instance count: " << MyString::getInstanceCount() << std::endl;
+
+    return 0;
+
+}
